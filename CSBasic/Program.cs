b52b34a9530a50ac89bb65a1faceb98922fc00f1
hyun031916 <hyun031916@gmail.com>
2021-04-16 10:21:25 +0900

    #2 c# 기본 용어 실습 한글 변수 되는 거 신기

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -8,8 +8,24 @@ namespace CSBasic
 {
     class Program
     {
+        string Var = "테스트";
         static void Main(string[] args)
         {
+            var name = "김" + "수" + "현";
+            Console.Write("개행 안 됨");
+            Console.WriteLine("개행 됨");
+
+            var alpha = "알파";
+            //var 0319 = "숫자 변수 안돼요";
+            var 김수현 = "한글 변수 된다";
+            //string ❤ = "이모지는 안 된다.";
+
+            // 인라인 주석
+            /*
+             * 여러 줄 주석
+             */
+
+         
         }
     }
 }
