7f39e260aa1857866a63e72183c235b334ab0368
hyun031916 <hyun031916@gmail.com>
2021-04-26 15:44:00 +0900

    #16 switch case로 계절 맞추기

--- CSBasic2/Program.cs
+++ CSBasic2/Program.cs
@@ -54,6 +54,42 @@ namespace CSBasic2
             {
                 Console.WriteLine("저녁 먹을 시간!");
             }
+
+            //switch 활용
+            Console.Write("숫자 입력하기: ");
+            int input3 = int.Parse(Console.ReadLine());
+            const int ZERO = 0;
+            const int ONE = 1;
+            switch(input3 % 2)
+            {
+                case ZERO:
+                    Console.WriteLine("짝수");
+                    break;
+                case ONE:
+                    Console.WriteLine("홀수");
+                    break;
+            }
+
+            Console.Write("오늘은 몇 월인가요: ");
+            int input4 = int.Parse(Console.ReadLine());
+            switch (input4)
+            {
+                case 12: case 1: case 2:
+                    Console.WriteLine("겨울");
+                    break;
+                case 3: case 4: case 5:
+                    Console.WriteLine("봄");
+                    break;
+                case 6: case 7: case 8:
+                    Console.WriteLine("여름");
+                    break;
+                case 9: case 10: case 11:
+                    Console.WriteLine("가을");
+                    break;
+                default:
+                    Console.WriteLine("제대로 입력하였는지 확인해주세요!");
+                    break;
+            }
         }
     }
 }
