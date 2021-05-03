36dbcf81b0e268b9b2db8301f5368f6462620769
hyun031916 <hyun031916@gmail.com>
2021-05-03 15:48:43 +0900

    #18 while 반복문/ do while 반복문 do while문은 답변이 맘에 들때까지 질문을 반복시킬 때 사용한당

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -22,6 +22,49 @@ namespace CSBasic3
             int[] intArray2 = new int[1000];
             Console.WriteLine(intArray2[0]);
             Console.WriteLine(intArray2[999]);
+
+            int i = 0;
+            int[] intArray3 = { 52, 273, 32, 65, 103 };
+            while(i < intArray3.Length)
+            {
+                Console.WriteLine(i + "번째 출력: " + intArray3[i]);
+                i++;
+            }
+
+            string input;
+            do
+            {
+                Console.Write("입력(exit을 입력하면 종료하기): ");
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("밥먹기");
+                        break;
+                    case "2":
+                        Console.WriteLine("양치하기");
+                        break;
+                    case "3":
+                        Console.WriteLine("씻기");
+                        break;
+                    case "4":
+                        Console.WriteLine("자기");
+                        break;
+                    default:
+                        Console.WriteLine("선택지가 없습니다. 입력값은 1~4입니다");
+                        break;
+                }
+            } while (input != "exit");
+            
+            /*for(int i2 = '가'; i2 <= '힣'; i2++)
+            {
+                Console.Write((char)i2);
+            }*/
+
+            for (int i3 = intArray3.Length -1; i >= 0; i--)
+            {
+                Console.WriteLine(intArray3[i3]);
+            }
          }
     }
 }
