f3c12fcfee1682304dd0a7667a82e21c36372d64
hyun031916 <hyun031916@gmail.com>
2021-08-30 15:13:20 +0900

    인덱서 테스트

--- CSBasic7/Program.cs
+++ CSBasic7/Program.cs
@@ -7,6 +7,30 @@ using System.Threading.Tasks;
 namespace CSBasic7
 {
 
+    class Products
+    {
+        private List<string> products = new List<string>();
+
+        public Products()
+        {
+            products.Add("아메리카노");
+            products.Add("카페라떼");
+            products.Add("딸기라떼");
+            products.Add("자몽허니블랙티");
+            products.Add("망고스무디");
+            products.Add("녹차오레오버블티");
+        }
+        public string this[int i]
+        {
+            get 
+            {
+                
+                return products[i];
+            }
+            set { Console.WriteLine(i + "번 째 상품 설정"); }
+        }
+    }
+
     class Wanted<T>
     {
         public T Value;
@@ -24,6 +48,9 @@ namespace CSBasic7
     {
         static void Main(string[] args)
         {
+            Products p = new Products();
+            Console.WriteLine(p[2]);
+            Console.WriteLine((new Products())[2]);
             Wanted<string> wantedString = new Wanted<string>("스트링");
             Wanted<int> wnatedInt = new Wanted<int>(3);
             Wanted<Student> wantedStudet = new Wanted<Student>(new Student());
