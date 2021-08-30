6a3f4e8f0979b46cd0ffdad63e69a1e771ce7f82
hyun031916 <hyun031916@gmail.com>
2021-08-30 14:46:48 +0900

    Generic 알아보기

--- CSBasic7/Program.cs
+++ CSBasic7/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace CSBasic7
 {
+
+    class Wanted<T>
+    {
+        public T Value;
+        public Wanted(T value)
+        {
+            this.Value = value;
+        }
+    }
+
+    class Student
+    {
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Wanted<string> wantedString = new Wanted<string>("스트링");
+            Wanted<int> wnatedInt = new Wanted<int>(3);
+            Wanted<Student> wantedStudet = new Wanted<Student>(new Student());
         }
     }
 }
